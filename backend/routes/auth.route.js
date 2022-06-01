792ca73deb05a27b1e09a1e985e533e52dd00814
Deekshant Yadav <d33kshant@gmail.com>
2022-06-01 18:31:36 +0530

    Renamed auth route

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user.model');
+const Creator = require('../models/creator.model');
+
+//Route Createuser
+
+router.post('/createuser', async (req, res) => {
+    const { name, phonenumber, email, dob, gender } = req.body;
+    let user = await User.create({
+        name,
+        phonenumber,
+        email,
+        dob,
+        gender
+    })
+    res.json(user);
+});
+router.post('/getuser', async (req, res) => {
+    const { phonenumber } = req.body;
+    let user = await User.findOne({ phonenumber });
+    if (user)
+        res.json(user);
+    else
+        res.send(false);
+});
+router.post('/createcreator', async (req, res) => {
+    const { name, phonenumber, username, email, domain, instagramusername, instagramcount, youtubeusername, youtubecount, facebookusername, facebookcount, twitterusername, twittercount } = req.body;
+    let creator = await Creator.create({
+        name,
+        phonenumber,
+        username,
+        email,
+        domain,
+        instagramusername,
+        instagramcount,
+        youtubeusername,
+        youtubecount,
+        facebookusername,
+        facebookcount,
+        twitterusername,
+        twittercount
+    })
+    res.json(creator);
+});
+router.post('/getcreator', async (req, res) => {
+    const { phonenumber } = req.body;
+    let creator = await Creator.findOne({ phonenumber });
+    if (creator)
+        res.json(creator);
+    else
+        res.send(false);
+});
+
+module.exports = router;
