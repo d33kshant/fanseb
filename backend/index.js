6ef0db14087d1187167bd225d9b1f835c98b25e0
Deekshant Yadav <d33kshant@gmail.com>
2022-06-01 10:03:22 +0530

    Added documentation and updated code

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,40 @@
+// Import enviroment variables from .env
 require('dotenv').config()
 
+// Import Dependencies
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+// Import Routes
 const authRoute = require('./routes/auth')
 
+// Constants
 const PORT = process.env.PORT || 5000
 const DB_URI = process.env.DB_URI
 
+// Create and configure express app
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+// Setup Routes
 app.use('/api/auth', authRoute)
 
+// Connect to database
 mongoose.connect(DB_URI, error => {
     if (error) {
         console.log('Failed to connect to database.')
     } else {
         console.log('Connected to database.')
+
+        // Start the server after connected to database
         const server = app.listen(PORT, () => {
             console.log('Server listening on port:', PORT)
         })
 
-        const gracefulShutdown = signal => {
+        // Graceful shutdown
+        ["SIGTERM", "SIGINT"].forEach(signal => {
             process.on(signal, async () => {
                 server.close()
                 await mongoose.disconnect()
@@ -32,8 +42,6 @@ mongoose.connect(DB_URI, error => {
                 console.log('Server Closed:', signal)
                 process.exit(0)
             })
-        }
-
-        ["SIGTERM", "SIGINT"].forEach(signal => gracefulShutdown(signal))
+        })
     }
 })
