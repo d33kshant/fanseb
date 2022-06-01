dc5eb730d461d1896a575bcfeb1e16af89754536
Deekshant Yadav <d33kshant@gmail.com>
2022-06-01 18:31:58 +0530

    Renamed creator model

---
+++ backend/models/creator.model.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const CrestorsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phonenumber: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    domain: {
+        type: String,
+        required: true
+    },
+    instagramusername: {
+        type: String
+    },
+    instagramcount: {
+        type: Number
+    },
+    youtubeusername: {
+        type: String
+    },
+    youtubecount: {
+        type: Number
+    },
+    facebookusername: {
+        type: String
+    },
+    facebookcount: {
+        type: Number
+    },
+    twitterusername: {
+        type: String
+    },
+    twittercount: {
+        type: Number
+    },
+})
+
+module.exports = mongoose.model('creators', CrestorsSchema);
