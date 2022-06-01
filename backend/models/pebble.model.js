a69cfd89ae01a32f71c9c41b414dc58cebf2c244
Deekshant Yadav <d33kshant@gmail.com>
2022-06-01 18:30:36 +0530

    Created model for pebble

---
+++ backend/models/pebble.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const PebbleSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	creator: {
+		type: String,
+		required: true,
+	},
+	video_url: {
+		type: String,
+		required: true,
+	},
+	created_on: {
+		type: Date,
+		default: Date.now,
+	},
+	products: [String]
+})
+
+module.exports = mongoose.model('pebble', PebbleSchema)
