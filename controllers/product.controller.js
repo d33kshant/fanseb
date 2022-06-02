3f2bd3d1806487696409c836705bddc8127dfa73
Deekshant Yadav <d33kshant@gmail.com>
2022-06-02 19:52:25 +0530

    Added function to get or update product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -27,6 +27,51 @@ const addProduct = async (req, res) => {
 	}
 }
 
+const updateProduct = async (req, res) => {
+	const {
+		id,
+		name,
+		category,
+		original_price,
+		selling_price,
+	} = req.body
+
+	try {
+		// TODO: check if creater is updating the product
+		await Product.findOneAndUpdate({ _id: id }, {
+			name,
+			category,
+			original_price,
+			selling_price,
+		})
+		res.json({
+			message: "Product has been updated."
+		})
+	} catch (error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
+}
+
+const getProducts = async (req, res) => {
+	const id = req.query.id || ""
+	const creator = req.query.c || ""
+
+	const filter = id ? { _id: id } : { creator }
+
+	try {
+		const products = await Product.find(filter)
+		res.json(products)
+	} catch (error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
+}
+
 module.exports = {
 	addProduct,
+	getProducts,
+	updateProduct,
 }
