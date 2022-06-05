998b95c5a8c7fe9ac3233a7addb32dd6a1be99bc
Deekshant Yadav <d33kshant@gmail.com>
2022-06-05 23:14:13 +0530

    Added route to add or delete product from collection

--- routes/collection.route.js
+++ routes/collection.route.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
-const { createCollection, deleteCollection } = require('../controllers/collection.controller')
+const { createCollection, deleteCollection, addProductInCollection, removeProductFromCollection } = require('../controllers/collection.controller')
 
 router.get('/')
 router.post('/', createCollection)
 router.delete('/', deleteCollection)
+
+router.post('/product', addProductInCollection)
+router.delete('/product', removeProductFromCollection)
 
 module.exports = router
