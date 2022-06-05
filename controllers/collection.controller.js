6d31ae479ebd4ab26aa5a3abf37c5f0f02f9ebc3
Deekshant Yadav <d33kshant@gmail.com>
2022-06-05 23:13:18 +0530

    Added function to add or delete product from collection

--- controllers/collection.controller.js
+++ controllers/collection.controller.js
@@ -41,7 +41,71 @@ const deleteCollection = async (req, res) => {
 	}
 }
 
+const addProductInCollection = async (req, res) => {
+	const { collection_id } = req.body.collection
+	const { product_id } = req.body.product
+
+	try {
+		const collection = await Collection.findById(collection_id)
+		if (collection) {
+			if (collection.products.indexOf(product_id) === -1) {
+				collection.products.push(product_id)
+				await collection.save()
+				res.json({
+					message: "New product has been added to your collection."
+				})
+			} else {
+				res.json({
+					error: "Product already exist in your collection."
+				})
+			}
+		} else {
+			res.json({
+				error: "Collection not found."
+			})
+		}
+	} catch (error) {
+		res.json({
+			error: "Something went wrong.",
+			payload: error,
+		})
+	}
+}
+
+const removeProductFromCollection = async (req, res) => {
+	const { collection_id } = req.body.collection
+	const { product_id } = req.body.product
+
+	try {
+		const collection = await Collection.findById(collection_id)
+		if (collection) {
+			if (collection.products.indexOf() !== -1) {
+				collection.products = collection.products.filter(product => product !== product_id)
+				await collection.save()
+				res.json({
+					message: "Product has been removed from your collection."
+				})
+			} else {
+				res.json({
+					error: "Product already removed or does not exist in your collection."
+				})
+			}
+		} else {
+			res.json({
+				error: "Collection not found."
+			})
+		}
+	} catch (error) {
+		res.json({
+			error: "Something went wrong.",
+			payload: error,
+		})
+	}
+}
+
 module.exports = {
 	createCollection,
-	deleteCollection
+	deleteCollection,
+	addProductInCollection,
+	removeProductFromCollection,
 }
