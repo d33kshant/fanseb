4a42f806959bb67e88fc289a39127a44ef45edb0
Deekshant Yadav <d33kshant@gmail.com>
2022-06-08 23:54:00 +0530

    Create header component for profile page

---
+++ frontend/src/components/CreatorProfileHeader.jsx
@@ -0,0 +1,24 @@
+export default function CreatorProfileHeader(creator) {
+
+	const user = null // TODO: authenticate current user
+
+	return (
+		<div className="profile-header-container">
+			<img src={creator.avatar} />
+			<div className="profile-info-container">
+				<div className="profile-info-top">
+					<h2 className="profile-info-name">{creator.name}</h2>
+					{ 
+						user !== creator ?
+						creator.inFollow ? 
+						<button className="profile-primary-action">Unfollow</button>
+						: <button className="profile-primary-action">Follow</button>
+						: <button className="profile-primary-action">Edit Profile</button>
+					}
+				</div>
+				<p className="profile-info-follower">{creator.follower} Followers</p>
+				<p className="profile-info-bio">{creator.bio}</p>
+			</div>
+		</div>
+	)
+}
