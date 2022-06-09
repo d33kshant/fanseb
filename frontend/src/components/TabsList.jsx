d0e95c6fddcd216ce39797906245b6966be06093
Deekshant Yadav <d33kshant@gmail.com>
2022-06-10 00:04:27 +0530

    Update TabList

--- frontend/src/components/TabsList.jsx
+++ frontend/src/components/TabsList.jsx
@@ -1,15 +1,15 @@
-function Tab({ title, onClick }) {
+function Tab({ active, title, onClick }) {
 	return (
-		<button onClick={onClick} className="tab-container">
+		<button style={{ color: active ? 'red' : 'gray' }} onClick={onClick} className="tab-container">
 			{ title }
 		</button>
 	)
 }
 
-export default function TabsList({tabs}){
+export default function TabsList({active, tabs}){
 	return (
 		<div className="tabs-list-container">
-			{tabs.map(tab=><Tab title={tab.title} onClick={tab.onClick} />)}
+			{tabs.map((tab, index)=><Tab key={index} active={index===active} title={tab.title} onClick={tab.onClick} />)}
 		</div>
 	)
 }
