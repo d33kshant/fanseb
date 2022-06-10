1c9e02ce2c7c7602e0280e95ddbeb443d6fe4f46
Deekshant Yadav <d33kshant@gmail.com>
2022-06-10 22:54:11 +0530

    Added element in navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,21 @@
+import React from "react"
+import "../styles/NavBar.css"
+
 export default function NavBar() {
-	return (
-		<div>
-			NavBar
-		</div>
-	)
+    return (
+        <div className="navbar-background">
+            <div className="navbar-container">
+                <div className="navbar-logo-container">FANSEB.</div>
+                <div className="navbar-search-container">
+                    <input
+                        className="navbar-search-input"
+                        placeholder="What are you looking for?"
+                    />
+                </div>
+                <div className="navbar-links-container">
+                    Bag WhishList Profile
+                </div>
+            </div>
+        </div>
+    )
 }
