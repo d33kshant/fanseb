5dedb6f6c54bef649a47afa1992dae99b97693ca
Deekshant Yadav <d33kshant@gmail.com>
2022-06-10 22:52:25 +0530

    Temporary disbaled extra routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Creatorlogin from './components/Creator/Login/Creatorlogin';
-import Creatorprofile from './components/Creator/Profile/Creatorprofile';
-import Userlogin from './components/User/Login/Userlogin';
-import Userprofile from './components/User/Profile/Userprofile';
+// import Creatorlogin from './components/Creator/Login/Creatorlogin';
+// import Creatorprofile from './components/Creator/Profile/Creatorprofile';
+// import Userlogin from './components/User/Login/Userlogin';
+// import Userprofile from './components/User/Profile/Userprofile';
 import { AuthProvider } from './context/FirebaseContext';
 import CreatorProfile from './pages/CreatorProfile';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div>
       <AuthProvider>
+        <NavBar />
         <Router>
           <Routes>
             <Route path='/' element={<CreatorProfile />} />
-            <Route exact path="/creatorlogin" element={<Creatorlogin />} />
+            {/* <Route exact path="/creatorlogin" element={<Creatorlogin />} />
             <Route exact path="/creatorprofile" element={<Creatorprofile />} />
             <Route exact path='/userlogin' element={<Userlogin />} />
-            <Route exact path='/userprofile' element={<Userprofile />} />
+            <Route exact path='/userprofile' element={<Userprofile />} /> */}
           </Routes>
         </Router>
       </AuthProvider>
