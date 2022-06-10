9a7b1f97e150f297edd6832ed43b819bd148a2ef
Deekshant Yadav <d33kshant@gmail.com>
2022-06-10 22:56:12 +0530

    Updated format and added active tab class name

--- frontend/src/components/TabsList.jsx
+++ frontend/src/components/TabsList.jsx
@@ -1,15 +1,28 @@
+import React from "react"
+import "../styles/TabList.css"
+
 function Tab({ active, title, onClick }) {
-	return (
-		<button style={{ color: active ? 'red' : 'gray' }} onClick={onClick} className="tab-container">
-			{ title }
-		</button>
-	)
+    return (
+        <button
+            onClick={onClick}
+            className={`tab-container ${active ? "active-tab" : ""}`}
+        >
+            {title}
+        </button>
+    )
 }
 
-export default function TabsList({active, tabs}){
-	return (
-		<div className="tabs-list-container">
-			{tabs.map((tab, index)=><Tab key={index} active={index===active} title={tab.title} onClick={tab.onClick} />)}
-		</div>
-	)
+export default function TabsList({ active, tabs }) {
+    return (
+        <div className="tabs-list-container">
+            {tabs.map((tab, index) => (
+                <Tab
+                    key={index}
+                    active={index === active}
+                    title={tab.title}
+                    onClick={tab.onClick}
+                />
+            ))}
+        </div>
+    )
 }
