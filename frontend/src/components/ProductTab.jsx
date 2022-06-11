e39212ed19b34844217bb61b6da40341b4fdc451
Deekshant Yadav <d33kshant@gmail.com>
2022-06-11 23:33:51 +0530

    Added sample products and product card

--- frontend/src/components/ProductTab.jsx
+++ frontend/src/components/ProductTab.jsx
@@ -1,7 +1,23 @@
-export default function ProductsTab() {
-	return (
-		<div>
-			Product's Tab
-		</div>
-	)
+import React from "react"
+import ProductCard from "./ProductCard"
+import "../styles/ProductTab.css"
+
+export default function ProductTab() {
+    const products = [
+        {
+            id: 100,
+            name: "Natural Vitamin C Serum",
+            image: "/avatar.png",
+            selling_price: 600,
+            original_price: 675,
+            category: "CS Essentials",
+        },
+    ]
+    return (
+        <div className="producr-grid-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} {...product} />
+            ))}
+        </div>
+    )
 }
