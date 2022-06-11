c17d4f0d21fd68b39996c7faef07cec7aa3dd958
Deekshant Yadav <d33kshant@gmail.com>
2022-06-11 23:31:52 +0530

    Added sample collection and collection card

--- frontend/src/components/CollectionTab.jsx
+++ frontend/src/components/CollectionTab.jsx
@@ -1,7 +1,30 @@
+import React from "react"
+import CollectionCard from "./CollectionCard"
+import "../styles/CollectionTab.css"
+
 export default function CollectionTab() {
-	return (
-		<div>
-			Collection's Tab
-		</div>
-	)
+    const collections = [
+        {
+            id: 0,
+            name: "Build your Travel Beauty Kit",
+            image: "https://via.placeholder.com/400",
+        },
+        {
+            id: 1,
+            name: "My Everyday Makeup Essentials ✨",
+            image: "https://via.placeholder.com/400",
+        },
+    ]
+    return (
+        <div className="collection-grid-container">
+            {collections.map((collection) => (
+                <CollectionCard
+                    key={collection.id}
+                    name={collection.name}
+                    image={collection.image}
+                    link={`/collection/${collection.id}`}
+                />
+            ))}
+        </div>
+    )
 }
