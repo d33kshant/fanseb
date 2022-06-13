5269ca84ce2bfaf36438ab26ce1ae2e4a8a2d1bf
Deekshant Yadav <d33kshant@gmail.com>
2022-06-13 23:29:43 +0530

    Updated image link and className

--- frontend/src/components/ProductTab.jsx
+++ frontend/src/components/ProductTab.jsx
@@ -7,14 +7,15 @@ export default function ProductTab() {
         {
             id: 100,
             name: "Natural Vitamin C Serum",
-            image: "/avatar.png",
+            image:
+                "https://images.unsplash.com/photo-1541643600914-78b084683601?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             selling_price: 600,
             original_price: 675,
             category: "CS Essentials",
         },
     ]
     return (
-        <div className="producr-grid-container">
+        <div className="product-grid-container">
             {products.map((product) => (
                 <ProductCard key={product.id} {...product} />
             ))}
