ca90a0345873dc7fad81e51105b7256f21d2e3a2
Deekshant Yadav <d33kshant@gmail.com>
2022-06-14 23:20:22 +0530

    Changed container to div fron link

--- frontend/src/components/PebbleCard.jsx
+++ frontend/src/components/PebbleCard.jsx
@@ -1,13 +1,12 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import "../styles/PebbleCard.css"
 
-export default function PebbleCard({ id, timestamp, image }) {
+export default function PebbleCard({ id, timestamp, image, onClick }) {
     return (
-        <Link className="pebble-card-container" to={`/pebble/${id}`}>
+        <div onClick={onClick} className="pebble-card-container" to={`/pebble/${id}`}>
             <img src={image} alt={`pebble ${id}`} className="pebble-image" />
             <div className="pebble-overlay"></div>
             <span className="pebble-timestamp">{timestamp}</span>
-        </Link>
+        </div>
     )
 }
