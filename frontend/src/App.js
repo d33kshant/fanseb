af0c8bc2b9f168a0f7f682086261e93dff362876
Deekshant Yadav <d33kshant@gmail.com>
2022-06-22 16:30:32 +0530

    Merged cart feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,14 @@ import { AuthProvider } from './context/FirebaseContext';
 import CreatorProfile from './pages/CreatorProfile';
 import NavBar from './components/NavBar'
 
+// Merge from cart
+import Cart from './components/Cart'
+import PaymentGateway from './components/PaymentGateway'
+import OrderStatus from './components/OrderStatus';
+import TrackOrder from './components/TrackOrder'
+// Merge end here
+
+
 function App() {
   return (
     <div>
@@ -21,6 +29,13 @@ function App() {
             <Route exact path="/creatorprofile" element={<Creatorprofile />} />
             <Route exact path='/userlogin' element={<Userlogin />} />
             <Route exact path='/userprofile' element={<Userprofile />} />
+
+            {/* Merge from cart */}
+            <Route exact path='/' element={<Cart />} />
+            {/* <Route path='/payment-gateway' element={<PaymentGateway />} /> */}
+            <Route path='/order-status' element={<OrderStatus />} />
+            <Route path='/track-order' element={<TrackOrder />} />
+            {/* Merge end here */}
           </Routes>
         </Router>
       </AuthProvider>
