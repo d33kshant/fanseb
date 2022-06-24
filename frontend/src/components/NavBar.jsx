dc1b353810c4fd6f72a0a490dbf1a9f30833b283
Deekshant Yadav <d33kshant@gmail.com>
2022-06-24 12:24:27 +0530

    Updated link's href

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -15,19 +15,19 @@ export default function NavBar() {
                     <input className="navbar-search-input" placeholder="What are you looking for?" />
                 </div>
                 <div className="navbar-links-container">
-                    <Link to="/cart">
+                    <Link to="/">
                         <svg viewBox="0 0 24 24" width="22" height="22" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <circle cx="9" cy="21" r="1"></circle>
                             <circle cx="20" cy="21" r="1"></circle>
                             <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
                         </svg>
                     </Link>
-                    <Link to="/whishlist">
+                    <Link to="/brand-dashboard">
                         <svg viewBox="0 0 24 24" width="22" height="22" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
                         </svg>
                     </Link>
-                    <Link to="/me">
+                    <Link to="/userprofile">
                         <svg viewBox="0 0 24 24" width="22" height="22" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                             <circle cx="12" cy="7" r="4"></circle>
