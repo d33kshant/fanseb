24de4bfacf1015263f91bbbbe87ca64ad1bd42ab
Deekshant Yadav <d33kshant@gmail.com>
2022-06-24 12:25:25 +0530

    Added new components from fanseb-master repo

---
+++ frontend/src/components/Creator/Brand-dashboard/BrandDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BrandDashboard = () => {
+  return (
+    <div className='card mx-auto my-5' style={{width:'70%'}}>
+        <div>hiuh</div>
+    </div>
+  )
+}
+
+export default BrandDashboard
