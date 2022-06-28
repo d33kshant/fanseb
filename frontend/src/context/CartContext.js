b366df2f037c9e05a27741a7adb64f094954558c
Deekshant Yadav <d33kshant@gmail.com>
2022-06-28 14:57:16 +0530

    Added temporary sample data

--- frontend/src/context/CartContext.js
+++ frontend/src/context/CartContext.js
@@ -1,13 +1,28 @@
-import { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const CartContext = createContext({ items: [], add: (item) => { }, remove: (item) => { } })
 
 const CartProvider = ({ children }) => {
 	// items is an array of objects with the following properties:
 	// id: string
+	// option: string
 	// quantity: number
 	// example: { id: '1234', quantity: 1 }
-	const [items, setItems] = useState([])
+	const [items, setItems] = useState([
+		{
+			id: 'abcd',
+			quantity: 3,
+		},
+		{
+			id: 'efgh',
+			quantity: 2
+		}
+	])
+
+	// Load cart item from user account
+	// useEffect(() => {
+	// TODO: fetch data from backend
+	// }, [])
 
 	const add = (item) => {
 		// find if item is already in cart
