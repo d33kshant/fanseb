fae736e16fb7a2c893d3d96f9e4c762980174513
Deekshant Yadav <d33kshant@gmail.com>
2022-06-29 21:14:28 +0530

    Restructured component

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
+import "../styles/CartItem.css"
 
-export default function CartItem({ id, count }) {
+export default function CartItem({ id, count, onAddClick, onRemoveClick }) {
     // const [item, setItem] = useState(null)
     const [loading, setLoading] = useState(false)
 
@@ -9,6 +10,7 @@ export default function CartItem({ id, count }) {
         original_price: 250,
         selling_price: 200,
         category: "Makeup",
+        image: "/cloth1.jpg",
     }
 
     // useEffect(() => {
@@ -23,22 +25,35 @@ export default function CartItem({ id, count }) {
 
     return (
         <div className="cart-item-container">
-            {!loading && item ? (
-                <>
-                    <div className="cart-item-name">{item.name}</div>
-                    <div className="cart-item-cateogry">{item.category}</div>
-                    <div className="cart-item-price-info">
-                        <div className="cart-item-price">{item.selling_price}</div>
-                        {discount > 0 && (
-                            <>
-                                <span className="cart-item-original-price">{item.original_price}</span> <span className="cart-item-discount">{discount}% Off</span>
-                            </>
-                        )}
-                    </div>
-                </>
-            ) : (
-                <p>Loading...</p>
-            )}
+            <img className="cart-item-image" src={item.image} alt="" />
+            <div className="cart-item-info">
+                {!loading && item ? (
+                    <>
+                        <div className="cart-item-name">{item.name}</div>
+                        <div className="cart-item-cateogry">{item.category}</div>
+                        <div className="cart-item-price-info">
+                            <div className="cart-item-price">₹{item.selling_price}</div>
+                            {discount > 0 && (
+                                <>
+                                    <span className="cart-item-original-price">₹{item.original_price}</span>
+                                    <span className="cart-item-discount">{discount}% Off</span>
+                                </>
+                            )}
+                        </div>
+                        <div className="cart-item-control">
+                            <button className="cart-item-add-button" onClick={onAddClick}>
+                                +
+                            </button>
+                            <span>{count}</span>
+                            <button className="cart-item-remove-button" onClick={onRemoveClick}>
+                                -
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
         </div>
     )
 }
