4bfc0b209d47987a2870c861b5e0882409b518e7
Deekshant Yadav <d33kshant@gmail.com>
2022-06-30 16:22:35 +0530

    Added class for styling purpose and restructured

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,8 +1,13 @@
 import React from "react"
+import { useContext } from "react"
+import { CartContext } from "../context/CartContext"
 import "../styles/ProductCard.css"
 
-export default function ProductCard({ name, image, category, link, selling_price, original_price }) {
+export default function ProductCard({ id, name, image, category, link, selling_price, original_price }) {
     if (!original_price) original_price = selling_price
+
+    const { add } = useContext(CartContext)
+
     return (
         <div className="product-card-container">
             <img className="product-image" src={image} alt={name} />
@@ -10,12 +15,12 @@ export default function ProductCard({ name, image, category, link, selling_price
                 <div className="product-category">{category}</div>
                 <h3 className="product-name">{name}</h3>
                 <div className="product-price-info">
-                    <span>{selling_price}</span>
-                    {original_price !== selling_price && <span>• {original_price}</span>}
-                    {original_price !== selling_price && <span>• {100 - Math.round((selling_price / original_price) * 100)}% Off</span>}
+                    <span className="product-selling-price">₹{selling_price}</span>
+                    {original_price !== selling_price && <span className="product-original-price"> ₹{original_price}</span>}
+                    {original_price !== selling_price && <span className="product-discount-value"> {100 - Math.round((selling_price / original_price) * 100)}% Off</span>}
                 </div>
             </div>
-            <button className="product-add-cart">
+            <button className="product-add-cart" onClick={() => add(id)}>
                 <svg viewBox="0 0 24 24" width="22" height="22" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <circle cx="9" cy="21" r="1"></circle>
                     <circle cx="20" cy="21" r="1"></circle>
