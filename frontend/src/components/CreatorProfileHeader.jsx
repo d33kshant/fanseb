a922211b3ff14a16ae633ea10287e6ae2b1d958e
Deekshant Yadav <d33kshant@gmail.com>
2022-06-30 16:23:08 +0530

    Formatted code

--- frontend/src/components/CreatorProfileHeader.jsx
+++ frontend/src/components/CreatorProfileHeader.jsx
@@ -6,50 +6,16 @@ export default function CreatorProfileHeader({ creator }) {
 
     return (
         <div className="profile-header-container">
-            <img
-                alt={creator.name}
-                className="profile-avatar-image"
-                src={creator.avatar}
-            />
+            <img alt={creator.name} className="profile-avatar-image" src={creator.avatar} />
             <div className="profile-info-container">
                 <div className="profile-info-top">
                     <h2 className="profile-info-name">{creator.name}</h2>
-                    {user !== creator ? (
-                        creator.inFollow ? (
-                            <button className="profile-primary-action action-top">
-                                Unfollow
-                            </button>
-                        ) : (
-                            <button className="profile-primary-action action-top">
-                                Follow
-                            </button>
-                        )
-                    ) : (
-                        <button className="profile-primary-action action-top">
-                            Edit Profile
-                        </button>
-                    )}
+                    {user !== creator ? creator.inFollow ? <button className="profile-primary-action action-top">Unfollow</button> : <button className="profile-primary-action action-top">Follow</button> : <button className="profile-primary-action action-top">Edit Profile</button>}
                 </div>
-                <p className="profile-info-follower">
-                    {creator.follower} Followers
-                </p>
+                <p className="profile-info-follower">{creator.follower || 0} Followers</p>
                 <p className="profile-info-bio">{creator.bio}</p>
             </div>
-            {user !== creator ? (
-                creator.inFollow ? (
-                    <button className="profile-primary-action action-bottom">
-                        Unfollow
-                    </button>
-                ) : (
-                    <button className="profile-primary-action action-bottom">
-                        Follow
-                    </button>
-                )
-            ) : (
-                <button className="profile-primary-action action-bottom">
-                    Edit Profile
-                </button>
-            )}
+            {user !== creator ? creator.inFollow ? <button className="profile-primary-action action-bottom">Unfollow</button> : <button className="profile-primary-action action-bottom">Follow</button> : <button className="profile-primary-action action-bottom">Edit Profile</button>}
         </div>
     )
 }
